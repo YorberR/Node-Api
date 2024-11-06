1b0dc313424623b8b845535f8251a62b6a9ca7bd
YorberR <yorberrojas15@gmail.com>
2024-11-06 16:49:21 -0400

    Categories

--- schema/schemaProduct.js
+++ schema/schemaProduct.js
@@ -3,15 +3,18 @@ const joi = require('joi');
 const id = joi.string().uuid();
 const name = joi.string().alphanum().min(3).max(30);
 const price = joi.number().integer().min(10).max(100000);
+const details = joi.string().min(10)
 
 const schemaProductCreate = joi.object({
   name: name.required(),
-  price: price.required()
+  price: price.required(),
+  details: details.required()
 });
 
 const updateShemaProduct = joi.object({
   name: name,
-  price: price
+  price: price,
+  details: details
 });
 
 const getProductSchema = joi.object({
